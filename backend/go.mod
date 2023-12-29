d842419f856e4d5dd2808e192611bb0721717a3e
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-29 12:33:04 +0100

    initial commit

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/gazes-media/gazes-novels
+
+go 1.21.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.backend = pkgs.mkShell {
+        packages = with pkgs; [
+          go_1_21_5
+          gotools
+        ];
+      };
+    };
+}
