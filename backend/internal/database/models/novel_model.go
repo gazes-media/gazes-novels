51a163b9bf55fa5ac685fbb0d6ebe71f52c099cb
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-30 12:42:13 +0100

    Add novel creation, chapter addition, and retrieval functions
    Implemented functions to create a new novel with its synopsis, add
    chapters to it, and retrieve novels and their chapters by ID or all
    novels.

--- backend/internal/database/models/novel_model.go
+++ backend/internal/database/models/novel_model.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/gazes-media/gazes-novels/internal/database"
 	"gorm.io/gorm"
 )
 
@@ -10,6 +11,53 @@ type Novel struct {
 	Synopsis string `json:"author"`
 
 	Chapters []Chapter `json:"chapters" gorm:"foreignKey:NovelID"`
+}
+
+func CreateNovel(title, synopsis string) (*Novel, error) {
+	novel := &Novel{
+		Title:    title,
+		Synopsis: synopsis,
+	}
+
+	if err := database.DB().Create(novel).Error; err != nil {
+		return nil, err
+	}
+
+	return novel, nil
+}
+
+func (n *Novel) AddChapter(title, content string) (*Chapter, error) {
+	chapter := &Chapter{
+		Title:   title,
+		Content: content,
+		NovelID: n.ID,
+	}
+
+	if err := database.DB().Create(chapter).Error; err != nil {
+		return nil, err
+	}
+
+	n.Chapters = append(n.Chapters, *chapter)
+
+	return chapter, nil
+}
+
+func GetNovelByID(id uint) (*Novel, error) {
+	var novel Novel
+	if err := database.DB().Preload("Chapters").First(&novel, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &novel, nil
+}
+
+func GetAllNovels() ([]Novel, error) {
+	var novels []Novel
+	if err := database.DB().Preload("Chapters").Find(&novels).Error; err != nil {
+		return nil, err
+	}
+
+	return novels, nil
 }
 
 type Chapter struct {
